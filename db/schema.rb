79b1464756d6166af6936bf920bda491226dd4ff
mascotguardian <pmaddison@gmail.com>
2019-04-26 04:06:14 +1000

    Integrated database relationship between topics and blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_040726) do
+ActiveRecord::Schema.define(version: 2019_04_25_174557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,15 +53,6 @@ ActiveRecord::Schema.define(version: 2019_04_25_040726) do
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "badge"
-  end
-
-  create_table "technologies", force: :cascade do |t|
-    t.string "name"
-    t.bigint "portfolio_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["portfolio_id"], name: "index_technologies_on_portfolio_id"
   end
 
   create_table "topics", force: :cascade do |t|
@@ -71,5 +62,4 @@ ActiveRecord::Schema.define(version: 2019_04_25_040726) do
   end
 
   add_foreign_key "blogs", "topics"
-  add_foreign_key "technologies", "portfolios"
 end
